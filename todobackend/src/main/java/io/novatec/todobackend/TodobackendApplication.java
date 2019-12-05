23fc5081eed2b8bf00854053549510ade1ee3f76
Matthias Haeussler <maeddes@gmail.com>
2019-12-05 06:09:19 +0100

    updates

--- todobackend/src/main/java/io/novatec/todobackend/TodobackendApplication.java
+++ todobackend/src/main/java/io/novatec/todobackend/TodobackendApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.Entity;
